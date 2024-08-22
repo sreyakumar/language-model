8b54413a26be4937fbe835623a8d7503c8067caa
Sreya Kumar <sreya.kumar@alleninstitute.org>
2024-08-22 22:07:07 +0000

    implemented projection retrieval

--- code/config.py
+++ code/config.py
@@ -1,42 +1,47 @@
 toolConfig = {
-  "tools": [
-      {
-      "toolSpec": {
-          "name": "one_doc_retrieval",
-          "description": "Retrieve one document from docDB. Only to be used if the prompt expresses certainty that one document should be accessed.",
-          "inputSchema": {
-              "json": {
-                  "type": "object",
-                  "properties": {
-                      "filter": {
-                          "type": "string",
-                          "description": "A MongoDB query to pass to the function"
+    "tools": [
+                      {
+                      "toolSpec": {
+                          "name": "doc_retrieval",
+                          "description": "Retrieve entire document from docDB. To be used only when it's necessary to retrieve all information in a document",
+                          "inputSchema": {
+                              "json": {
+                                  "type": "object",
+                                  "properties": {
+                                      "filter": {
+                                          "type": "string",
+                                          "description": "A MongoDB query to pass to the function"
+                                      }
+                                  },
+                                  "required": ["filter"]
+                              }
+                          }
                       }
-                  },
-                  "required": ["filter"]
-              }
-          }
-      }
-      },
-      {
-      "toolSpec": {
-          "name": "multi_doc_retrieval",
-          "description": "Retrieve multiple documents from docDB. Used in cases where multiple documents need to be retrieved",
-          "inputSchema": {
-              "json": {
-                  "type": "object",
-                  "properties": {
-                      "filter": {
-                          "type": "string",
-                          "description": "A MongoDB query to pass to the function"
+                      },
+                      {
+                      "toolSpec": {
+                          "name": "projection_retrieval",
+                          "description": "Retrieve multiple documents from docDB with only specific field information. Used when most of the document is not necessary to answer natural language query",
+                          "inputSchema": {
+                              "json": {
+                                  "type": "object",
+                                  "properties": {
+                                      "filter": {
+                                          "type": "string",
+                                          "description": "A MongoDB query to pass to the function"
+                                      },
+                                      "fieldNameList": {
+                                          "type": "string",
+                                          "description": "A list of field names following JSON format to retrieve from the document. The string shouldn't contain the value, just the key you would need to access the value in a metadata schema document."
+                                      }
+                                  },
+                                  "required": ["filter", "fieldNameList"]
+                              }
+                          }
                       }
-                  },
-                  "required": ["filter"]
-              }
-          }
-      }
-      }
-  ]
-}
-
-
+                      }
+                  ],
+                    "toolChoice": {
+                    "auto":{},
+                    }
+                }
